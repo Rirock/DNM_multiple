d3f3572e940b99d520667c339f025e4eaa69267d
ReLU_加一 <43679103+Rirock@users.noreply.github.com>
2023-02-20 15:34:08 +0900

    Add files via upload

--- DNM_model/DNM_models.py
+++ DNM_model/DNM_models.py
@@ -27,10 +27,10 @@ class DNM_Linear_M3(nn.Module):
         x = torch.unsqueeze(x, 1)
         x = torch.unsqueeze(x, 2)
         x = x.repeat(1, out_size, M, 1)
-        x = 0.5 * (torch.mul(x, self.params['Synapse_W']) - self.params['Synapse_q'])
+        x = torch.mul(x, self.params['Synapse_W']) - self.params['Synapse_q']
         x = torch.sigmoid(x)
 
-        x = torch.mul(x, self.params['Dendritic_W2'])
+        x = torch.mul(x, torch.tanh(self.params['Dendritic_W2']))
 
         # Dendritic
         x = torch.sum(x, 3) #prod 
@@ -40,7 +40,7 @@ class DNM_Linear_M3(nn.Module):
         x = torch.sum(x, 2)
 
         # Soma
-        x = self.params['k'] * (x - self.params['qs'])
+        #x = self.params['k'] * (x - self.params['qs'])
 
         return x
 
@@ -70,6 +70,26 @@ class DNM_multiple(nn.Module):
         for w in self.parameters():
             w.data.uniform_(-std, std)
 
+class DNM_multiple_3(nn.Module):
+    def __init__(self, input_size, hidden_size, out_size, M=5):
+        super(DNM_multiple_3, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.DNM_Linear_i = DNM_Linear_M3(input_size, hidden_size, M)
+        self.DNM_Linear_h = DNM_Linear_M3(hidden_size, hidden_size, M)
+        self.DNM_Linear_o = DNM_Linear_M3(hidden_size, out_size, M)
+    
+    def forward(self, x):
+        x = x.view(-1, self.input_size)
+        x = self.DNM_Linear_i(x)
+        x = self.DNM_Linear_h(x)
+        out = self.DNM_Linear_o(x)
+        return out
+    
+    def reset_parameters(self):
+        std = 1.0 / math.sqrt(self.hidden_size)
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
 
 class MLP(nn.Module):
     def __init__(self, input_size, hidden_size, out_size):
